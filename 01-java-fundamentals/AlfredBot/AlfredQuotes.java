d0fde6a2c78ad66582af531e09aad5cb9d64e50c
Nghuynh07 <79157586+Nghuynh07@users.noreply.github.com>
2021-08-02 15:40:59 -0700

    Corrected guestGreeting

--- 01-java-fundamentals/AlfredBot/AlfredQuotes.java
+++ 01-java-fundamentals/AlfredBot/AlfredQuotes.java
@@ -1,5 +1,15 @@
 import java.util.Date;
 public class AlfredQuotes {
 
+    public String guestGreeting(String name, String dayPeriod) {
+        String greeting = String.format("Good %s %s, Lovely to see you.", dayPeriod, name);
+        return greeting;
+    }
+
+    public String respondBeforeAlexis(String conversation) {
+
+        return "place holder for snarky response return string";
+    }
 
 }
+
--- 01-java-fundamentals/AlfredBot/AlfredTest.java
+++ 01-java-fundamentals/AlfredBot/AlfredTest.java
@@ -1,5 +1,11 @@
 public class AlfredTest {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+    AlfredQuotes alfredBot = new AlfredQuotes();
+    String testGuestGreeting = alfredBot.guestGreeting("Beth Kane", "evening");
+    System.out.println(testGuestGreeting);
+
+
+
 
     }
 }
