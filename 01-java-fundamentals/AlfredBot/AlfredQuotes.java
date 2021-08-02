ea73e0fa771960f5e47a985a26d1bbcd457a30e9
Nghuynh07 <79157586+Nghuynh07@users.noreply.github.com>
2021-08-02 14:02:00 -0700

    Alfred Quotes Assignment

---
+++ 01-java-fundamentals/AlfredBot/AlfredQuotes.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+public class AlfredQuotes {
+    public String guestGreeting(String name, String dayPeriod){
+        String personGreeting = String.format("hi %s, %s sir!");
+        return personGreeting;
+    }
+
+
+
+}
---
+++ 01-java-fundamentals/AlfredBot/AlfredTest.java
@@ -0,0 +1,9 @@
+public class AlfredTest {
+    public static void main(String[] args){
+        AlfredQuotes alfredBot = new AlfredQuotes();
+        String testGreeting = alfredBot.guestGreeting("Tony Truong", "evening");
+        System.out.println(testGreeting);
+
+
+    }
+}
