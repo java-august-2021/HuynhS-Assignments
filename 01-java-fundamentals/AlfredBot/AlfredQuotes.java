cbe7083fdca75a1eb55359dfc21cb4814f0a9f7d
Nghuynh07 <79157586+Nghuynh07@users.noreply.github.com>
2021-08-02 14:06:11 -0700

    .

--- 01-java-fundamentals/AlfredBot/AlfredQuotes.java
+++ 01-java-fundamentals/AlfredBot/AlfredQuotes.java
@@ -1,10 +1,5 @@
 import java.util.Date;
 public class AlfredQuotes {
-    public String guestGreeting(String name, String dayPeriod){
-        String personGreeting = String.format("hi %s, %s sir!");
-        return personGreeting;
-    }
-
 
 
 }
--- 01-java-fundamentals/AlfredBot/AlfredTest.java
+++ 01-java-fundamentals/AlfredBot/AlfredTest.java
@@ -1,9 +1,5 @@
 public class AlfredTest {
     public static void main(String[] args){
-        AlfredQuotes alfredBot = new AlfredQuotes();
-        String testGreeting = alfredBot.guestGreeting("Tony Truong", "evening");
-        System.out.println(testGreeting);
-
 
     }
 }
