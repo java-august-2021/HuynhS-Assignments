5d2436fc14d32b8f2ecef447300b6dff36af2307
Nghuynh07 <79157586+Nghuynh07@users.noreply.github.com>
2021-08-03 16:52:48 -0700

    FizzBuzz turn in

--- 01-java-fundamentals/03-fizzbuzz/FizzBuzz.java
+++ 01-java-fundamentals/03-fizzbuzz/FizzBuzz.java
@@ -1,5 +1,13 @@
 public class FizzBuzz {
     public String fizBuzz(int number){
-        
+        if( number % 3 == 0){
+            return "Fizz";
+        }else if(number % 5 == 0){
+            return "Buzz";
+        }else if (number % 3 == 0 && number % 5 == 0){
+            return "FizzBuzz";
+        }else{
+            return Integer.toString(number);
+        }
     }
 }
---
+++ 01-java-fundamentals/03-fizzbuzz/TestFizzBuzz.java
@@ -0,0 +1,16 @@
+public class TestFizzBuzz {
+    public static void main(String[] args){
+        FizzBuzz tester = new FizzBuzz();
+
+        System.out.println(tester.fizBuzz(9));
+        System.out.println(tester.fizBuzz(10));
+        System.out.println(tester.fizBuzz(16));
+        System.out.println(tester.fizBuzz(16));
+        System.out.println(tester.fizBuzz(2));
+
+
+        for(int i = 0; i < 200; i++){
+            System.out.println(tester.fizBuzz(i));
+        }
+    }
+}
