c2b3df23b1905039d1fb82f7b1799264713a28f4
Nghuynh07 <79157586+Nghuynh07@users.noreply.github.com>
2021-08-05 06:50:16 -0700

    .

--- 01-java-fundamentals/03-fizzbuzz/TestFizzBuzz.java
+++ 01-java-fundamentals/03-fizzbuzz/TestFizzBuzz.java
@@ -7,6 +7,7 @@ public class TestFizzBuzz {
         System.out.println(tester.fizBuzz(16));
         System.out.println(tester.fizBuzz(16));
         System.out.println(tester.fizBuzz(2));
+        System.out.println(tester.fizBuzz(33));
 
 
         for(int i = 0; i < 200; i++){
