be591a2f79d36e23730316fa078060cd0706c946
Nghuynh07 <79157586+Nghuynh07@users.noreply.github.com>
2021-08-17 17:03:40 -0700

    test

---
+++ stringassignment/src/main/java/com/example/stringassignment/StringassignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.stringassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringassignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringassignmentApplication.class, args);
+	}
+
+}
